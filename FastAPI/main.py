e861c94099bc189c4e4cb990aab926f2fd22fab8
Eliz <skybear.al@gmail.com>
2025-09-29 13:39:17 +0200

    got it to work

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -6,22 +6,37 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+# class Item(BaseModel):
+#     name: str
+#     price: float
+#     is_offer: Union[bool, None] = None
+#
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, item: Item):
+#     return {"item_name": item.name, "item_id": item_id}
 
+@app.get("/add")
+def add(a: float, b: float):
+    return {"result": a + b}
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/subtract")
+def subtract(a: float, b: float):
+    return {"result": a - b}
+
+@app.get("/multiply")
+def multiply(a: float, b: float):
+    return {"result": a * b}
+
+@app.get("/divide")
+def divide(a: float, b: float):
+    return {"result": a / b}
